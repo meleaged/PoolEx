b3299bf5c9ef93d837ccac6c0023bcb96181dfba
meleaged <meleaged@student.21-school.ru>
2024-08-29 14:59:35 +0300

    Update 2nextvar.c

--- Exam/2nextvar.c
+++ Exam/2nextvar.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-
-void capitalize(char *str) {
-int capitalize_next = 1; // Флаг для обозначения необходимости капитализации следующего символа
-
-for (int i = 0; str[i] != '\0'; i++) {
-if (str[i] == ' ') {
-capitalize_next = 1; // Если символ - пробел, то следующий должен быть заглавным
-} else if (capitalize_next && str[i] >= 'a' && str[i] <= 'z') {
-str[i] = str[i] - ('a' - 'A'); // Переводим в верхний регистр
-capitalize_next = 0; // Отключаем флаг до следующего пробела
-} else {
-capitalize_next = 0; // Если символ не пробел, сбрасываем флаг
-}
-}
-}
 
 int main() {
 char str[100];
+int capitalize_next = 1;
 
 fgets(str, sizeof(str), stdin);
 
-capitalize(str);
+for (int i = 0; str[i] != '\0'; i++) {
+if (str[i] == ' ') {
+capitalize_next = 1;
+} else if (capitalize_next && str[i] >= 'a' && str[i] <= 'z') {
+str[i] = str[i] - ('a' - 'A');
+capitalize_next = 0;
+} else {
+capitalize_next = 0;
+}
+}
 
 printf("%s", str);
 
-return
+return 0;
+}
